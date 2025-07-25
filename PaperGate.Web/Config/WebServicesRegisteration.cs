4f6b440c85af7328f76ae82731b72fe7e29e9e56
Mohammad Mahdi Zarei <96488897+masterZarei@users.noreply.github.com>
2025-07-25 19:06:19 +0330

    Updated Packages.

--- PaperGate.Web/Config/WebServicesRegisteration.cs
+++ PaperGate.Web/Config/WebServicesRegisteration.cs
@@ -8,7 +8,7 @@ public static class WebServicesRegisteration
     public static void RegisterServices(this IServiceCollection service)
     {
         service.AddScoped<IFileManagementService, FileManagementService>();
-        service.AddAutoMapper(typeof(MappingProfile).Assembly);
+        service.AddAutoMapper(cfg => { }, typeof(MappingProfile).Assembly);
 
     }
 }
