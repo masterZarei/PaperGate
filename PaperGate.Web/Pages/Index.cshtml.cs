e0c711bfe4410e08dd9fca6409a1035f48079c8a
Mohammad Mahdi Zarei <96488897+masterZarei@users.noreply.github.com>
2025-08-06 17:34:51 +0330

    Index Page backend is Almost done.

--- PaperGate.Web/Pages/Index.cshtml.cs
+++ PaperGate.Web/Pages/Index.cshtml.cs
@@ -1,18 +1,53 @@
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PaperGate.Core.Interfaces;
+using PaperGate.Web.Utilities.Helpers;
+using PaperGate.Web.ViewModels;
+using ILogger = Serilog.ILogger;
 
 namespace PaperGate.Web.Pages;
 
-public class IndexModel : PageModel
+public class IndexModel : MyPageModel
 {
-    private readonly ILogger<IndexModel> _logger;
+    private readonly ILogger _logger;
+    private readonly IUnitOfWork _unitOfWork;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger logger, IUnitOfWork unitOfWork)
     {
         _logger = logger;
+        _unitOfWork = unitOfWork;
     }
+    public IndexDto IndexDto { get; set; }
 
-    public void OnGet()
+    public async Task<IActionResult> OnGet()
     {
+        try
+        {
+            string AddOnHeadPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/", "Html/AddOnHead.html");
+            string AddOnBodyPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/", "Html/AddOnBody.html");
+            string AddOnAfterBodyPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/", "Html/AddOnAfterBody.html");
+
+
+            IndexDto = new IndexDto
+            {
+                Posts = await _unitOfWork.Post.GetAllReadOnlyAsync(b => b.IsActive, queryCustomizer: b => b.Include(b => b.Author).OrderByDescending(b => b.CreatedOn).Take(6)),
+                Sliders = await _unitOfWork.Post.GetAllReadOnlyAsync(p=>p.ShowOnSlider && p.IsActive),
+            };
+            if (System.IO.File.Exists(AddOnHeadPath))
+                IndexDto.AddOnHeadHtml = System.IO.File.ReadAllText(AddOnHeadPath);
+            if (System.IO.File.Exists(AddOnBodyPath))
+                IndexDto.AddOnBodyHtml = System.IO.File.ReadAllText(AddOnBodyPath);
+            if (System.IO.File.Exists(AddOnAfterBodyPath))
+                IndexDto.AddOnAfterBodyHtml = System.IO.File.ReadAllText(AddOnAfterBodyPath);
+            return Page();
+
+        }
+        catch (Exception ex)
+        {
+            _logger.Fatal(ex, "DANGER DANGER DANGER INDEX FAILED ON ONGET", IndexDto);
+            ShowError(ErrorMessages.ERRORHAPPEDNED);
+            return Content("خطایی رخ داد");
+        }
 
     }
 }
---
+++ PaperGate.Web/ViewModels/IndexDto.cs
@@ -0,0 +1,14 @@
+﻿using PaperGate.Core.Entities;
+
+namespace PaperGate.Web.ViewModels;
+
+public class IndexDto
+{
+    public IReadOnlyList<PostInfo>? Sliders { get; set; }
+   // public BannerInfo? Banner { get; set; }
+    public IReadOnlyList<PostInfo>? Posts { get; set; }
+
+    public string? AddOnHeadHtml { get; set; }
+    public string? AddOnBodyHtml { get; set; }
+    public string? AddOnAfterBodyHtml { get; set; }
+}
