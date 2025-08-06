f84c64904db3856739cbb65ff1f693471a9cb822
Mohammad Mahdi Zarei <96488897+masterZarei@users.noreply.github.com>
2025-08-06 17:43:13 +0330

    Refactored unitofwork To use LazyLoading instead of EagerLoading.

--- PaperGate.Infra/Implementations/UnitOfWork.cs
+++ PaperGate.Infra/Implementations/UnitOfWork.cs
@@ -16,6 +16,52 @@ public class UnitOfWork : IUnitOfWork
     private readonly ILogger _myLogger;
     protected TaskResult _taskResult;
 
+    private IPostRepository _post;
+    private IGenericRepository<CategoryInfo> _category;
+    private IGenericRepository<KeywordInfo> _keyword;
+    private IGenericRepository<PostKeywordInfo> _paperKeyword;
+    private IGenericRepository<MessageInfo> _message;
+    private IGenericRepository<AboutUsInfo> _aboutUs;
+    private IGenericRepository<ContactWayInfo> _contactWay;
+
+    public UnitOfWork(AppDbContext db, ILogger myLogger)
+    {
+        _db = db;
+        _myLogger = myLogger;
+        _taskResult = new();
+    }
+
+    public IPostRepository Post => _post ??= new PostRepository(_db, _myLogger);
+    public IGenericRepository<CategoryInfo> Category => _category ??= new GenericRepository<CategoryInfo>(_db, _myLogger);
+    public IGenericRepository<KeywordInfo> Keyword => _keyword ??= new GenericRepository<KeywordInfo>(_db, _myLogger);
+    public IGenericRepository<PostKeywordInfo> PaperKeyword => _paperKeyword ??= new GenericRepository<PostKeywordInfo>(_db, _myLogger);
+    public IGenericRepository<MessageInfo> Message => _message ??= new GenericRepository<MessageInfo>(_db, _myLogger);
+    public IGenericRepository<AboutUsInfo> AboutUs => _aboutUs ??= new GenericRepository<AboutUsInfo>(_db, _myLogger);
+    public IGenericRepository<ContactWayInfo> ContactWay => _contactWay ??= new GenericRepository<ContactWayInfo>(_db, _myLogger);
+
+    public async Task<TaskResult> SaveChangesAsync()
+    {
+        try
+        {
+            await _db.SaveChangesAsync();
+            _taskResult.Succeeded = true;
+            return _taskResult;
+        }
+        catch (Exception ex)
+        {
+            _taskResult.AddError(ex.ToString());
+            _myLogger.Fatal(ex, "UnitOfWork-SaveChanges", LoggingLevel.Fatal);
+            return _taskResult;
+        }
+    }
+}
+
+/*public class UnitOfWork : IUnitOfWork
+{
+    private readonly AppDbContext _db;
+    private readonly ILogger _myLogger;
+    protected TaskResult _taskResult;
+
     public UnitOfWork(AppDbContext db, ILogger myLogger)
     {
         _db = db;
@@ -54,3 +100,4 @@ public class UnitOfWork : IUnitOfWork
         }
     }
 }
+*/
