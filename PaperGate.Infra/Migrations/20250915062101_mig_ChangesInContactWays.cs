3195ee235b109f1aa2b64ee60ccb062d06d45a4a
Mohammad Mahdi Zarei <96488897+masterZarei@users.noreply.github.com>
2025-09-15 11:46:28 +0330

    Footer Latinization is Completed.

---
+++ PaperGate.Infra/Migrations/20250915062101_mig_ChangesInContactWays.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PaperGate.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_ChangesInContactWays : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "EnglishTitle",
+                table: "ContactWays",
+                newName: "EnglishLink");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "EnglishLink",
+                table: "ContactWays",
+                newName: "EnglishTitle");
+        }
+    }
+}
