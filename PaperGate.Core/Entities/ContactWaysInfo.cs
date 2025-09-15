4ccb046b9edbd08903b78a58e354e7f69dd5663b
Mohammad Mahdi Zarei <96488897+masterZarei@users.noreply.github.com>
2025-09-15 09:42:51 +0330

    mig_AddedEnglishTitleForContactWays

--- PaperGate.Core/Entities/ContactWaysInfo.cs
+++ PaperGate.Core/Entities/ContactWaysInfo.cs
@@ -2,6 +2,7 @@
 public class ContactWayInfo : BaseEntity
 {
     public string Title { get; set; }
+    public string? EnglishTitle { get; set; } = string.Empty;
     public string? Link { get; set; }
     public string? Icon { get; set; }
     public bool IsLink { get; set; }
--- PaperGate.Infra/Migrations/20250915061229_mig_AddedEnglishTitleForContactWays.Designer.cs
+++ PaperGate.Infra/Migrations/20250915061229_mig_AddedEnglishTitleForContactWays.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PaperGate.Infra.Data;
 
@@ -11,9 +12,11 @@ using PaperGate.Infra.Data;
 namespace PaperGate.Infra.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250915061229_mig_AddedEnglishTitleForContactWays")]
+    partial class mig_AddedEnglishTitleForContactWays
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
